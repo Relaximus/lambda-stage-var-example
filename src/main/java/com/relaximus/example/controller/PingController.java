e4229cb6a693176a73cb3e23ded76fb808e69494
alche <a.chentsov@avax.de>
2019-01-31 10:12:47 +0100

    example

---
+++ src/main/java/com/relaximus/example/controller/PingController.java
@@ -0,0 +1,23 @@
+package com.relaximus.example.controller;
+
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@RestController
+@EnableWebMvc
+public class PingController {
+    static String MY_URL = "my url";
+
+    @RequestMapping(path = "/ping", method = RequestMethod.GET)
+    public Map<String, String> ping() {
+        Map<String, String> pong = new HashMap<>();
+        pong.put("pong", "Hello, World!");
+        pong.put("my-url", MY_URL);
+        return pong;
+    }
+}
